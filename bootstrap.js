73dff7851387562759ac7ea6037dcac11d10a33a
Jon Cooper <joncooper@carbonfive.com>
2011-08-26 07:26:12 -0700

    remove unused bootstrap js

--- bootstrap.js
+++
@@ -1,8 +0,0 @@
-
-chrome.tabs.executeScript(null, { code: "document.body.style.background='red !important'" });
-
-chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.tabs.executeScript(null, { file: "ptcx.js" });
-});
-
-
